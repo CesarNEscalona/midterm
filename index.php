a68791dc40198c915a645b080dcfe1671b09a4a1
Cesar Escalona <cesarescalona10@gmail.com>
2021-05-15 16:04:31 -0400

    Defined a survey route to index and displayed a checkbox list using controller

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ error_reporting(E_ALL);
 
 // Require autoload file
 require_once ('vendor/autoload.php');
+require_once('model/data-layer.php');
 
 // Start a session
 session_start();
@@ -21,6 +22,31 @@ $f3->route('GET /', function(){
     // Display the home page
     $view = new Template();
     echo $view->render('views/home.html');
+});
+
+$f3->route('GET|POST /survey', function($f3){
+    // Reinitialize the session array
+    $_SESSION = array();
+
+    // initialize variables to store user input
+    $userOptions = array();
+    $userName = "";
+
+    //If the form has been submitted, grab the data
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $userName = $_POST['name'];
+    }
+
+    //Get the options from the Model and send them to the View
+    $f3->set('options', getOptions());
+
+    // Add the data to the hive
+    $f3->set('userOptions', $userOptions);
+    $f3->set('userName', $userName);
+
+    // Display the home page
+    $view = new Template();
+    echo $view->render('views/survey.html');
 });
 
 // Run Fat-Free
