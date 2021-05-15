5dcfeeecac0580cedbffa6fd9a35bb61f5955ac8
Cesar Escalona <cesarescalona10@gmail.com>
2021-05-15 16:05:07 -0400

    Forgot to add the model, which features options for html page

---
+++ model/data-layer.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Return data for the survey file
+ */
+
+function getOptions()
+{
+    return array("This midterm is easy",
+        "I like midterms",
+        "Today is Saturday",
+        "It's Friday",
+        "Is this supposed to be painful?");
+}
